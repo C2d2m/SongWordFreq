7558870ba6cc3207aaf992c5a30c91aeffaba2b4
Caleb <CDM244@usask.ca>
2022-04-09 23:29:34 -0600

    finished string parsing and made wiki api call better

--- main.py
+++ main.py
@@ -4,33 +4,41 @@
 from bs4 import BeautifulSoup
 import wikipediaapi
 import requests
+import re
 
 
 def get_artist_name():
-    return input("Enter the artist name").replace(" ", '_')
+    return input("Enter the artist name: ").strip().replace(" ", '_')
 
 
 def pull_data(artist):
+    # TODO make way more robust
+
+
     wiki_wiki = wikipediaapi.Wikipedia('en')
 
-    artistPage = wiki_wiki.page(artist).text
+    artistPage = wiki_wiki.page(artist)
+
+    if not artistPage.exists():
+        print("Sorry, this artist cannot be found")
+        exit(-1)
+
+    artistPage = artistPage.text
 
     # Reformat artist name for Genius url
     artist = artist.replace('_', '-')
 
-    discog = artistPage[artistPage.find('Discography'):].split('\n')
-    discog.remove('Discography')
+    discog = artistPage[artistPage.find('Discography\nStudio albums'):].split('\n')
+    del discog[:3]
 
     albums = {}
-
     for line in discog:
         # Fills albums with genius URL formatted names of each studio album found in wikipedia api call
         if line == '':
             break
-        if line.__contains__('Studio albums'):
-            line = line.replace('Studio albums', '')
 
         albums[(line[:line.rfind('(') - 1].strip().replace(' ', '-'))] = {}
+
 
     for cAlbum in albums.keys():
         # Loop through all albums to get a list of songs
@@ -43,6 +51,8 @@ def pull_data(artist):
         songs = {}
         songDiv = soup.findAll('div', class_='chart_row-content')
         for cSong in songDiv:
+            # Loop for all the songs of the album to get their lyrics
+
             temp = cSong.text.strip()
             songName = temp[:temp.rfind('\n')]
 
@@ -53,19 +63,40 @@ def pull_data(artist):
 
             lyricDiv = songSoup.select('div[class*="Lyrics__Container"]')
 
+            # If there are no lyrics, go to next song, else begin parsing the string
             if len(lyricDiv) == 0:
                 print('There are no lyrics available for this song')
+                songs[songName] = None
                 continue
 
-            print(lyricDiv[0].text)
+            lyrics = lyricDiv[0].text
 
-            lyrics = ''
+            # Disgusting parsing
+            lyrics = re.sub(r'\[([A-Za-z0-9_:\-$&!@* ]+)]', '', lyrics)
+
+            # Leaving this here for now just in case i missed anything in above regex
+            if lyrics.__contains__('['):
+                print('wtf')
+                print(lyrics)
+                exit()
+
+            changes = 0
+            toChange = []
+            for i in range(1, len(lyrics) - 1):
+                if lyrics[i].isupper() and lyrics[i-1] != " ":
+                    toChange.append(i + changes)
+                    changes += 1
+
+            for i in toChange:
+                lyrics = lyrics[:i] + ' ' + lyrics[i:]
+
             songs[songName] = lyrics
 
-        #albums[cAlbum]['songs'] = songs
+        albums[cAlbum]['songs'] = songs
         #print(albums[cAlbum])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pull_data('Yung_Lean')
+
+    pull_data(get_artist_name())
